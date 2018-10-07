59b107a8d885254ed9018a8277ca38d74119dddd
LeslieInostroza <leslie.ino@gmail.com>
2018-10-07 15:55:11 -0300

    menu listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MenuNavbarComponent } from './menu-navbar/menu-navbar.component';
+import { PaginaMuroComponent } from './pagina-muro/pagina-muro.component';
 
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: PaginaMuroComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuNavbarComponent,
+    PaginaMuroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
